545d7397bb0a9c73a51347142dbe72f15169674f
timmybea <tim.beals@gmail.com>
2018-02-05 13:41:18 -0800

    first commit of new project

---
+++ InterstitialAdTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InterstitialAdTest
+//
+//  Created by Tim Beals on 2018-02-05.
+//  Copyright © 2018 Tim Beals. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
